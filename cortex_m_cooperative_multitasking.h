245403a929cccb708e154242c06c181c9b947473
Richard Campbell <rlcamp@users.noreply.github.com>
2025-08-28 06:40:03 -0700

    expand isc license text

--- cortex_m_cooperative_multitasking.h
+++ cortex_m_cooperative_multitasking.h
@@ -1,3 +1,17 @@
+/* Copyright 2021-2025 Richard Campbell
+
+ Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ without fee is hereby granted, provided that the above copyright notice and this
+ permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
+ USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 struct child_context {
     /* the context switching macros expect this to immediately follow the stack space */
     unsigned char context[4] __attribute((aligned(8)));
