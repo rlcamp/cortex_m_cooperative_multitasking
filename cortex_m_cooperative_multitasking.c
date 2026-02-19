c331d6d161d4ee842bb2aae316495d66c11206a5
Richard Campbell <rlcamp@users.noreply.github.com>
2026-02-18 19:15:16 -0800

    context switch directly to next child instead of back to main

--- cortex_m_cooperative_multitasking.c
+++ cortex_m_cooperative_multitasking.c
@@ -16,53 +16,52 @@
 
 /* these macros are derived from the armv7-m path in https://github.com/rlcamp/coroutine */
 
-#define BOOTSTRAP_CONTEXT(buf, func) do { \
-register void * _buf asm("r0") = buf; /* ensure the compiler places this where it will be the argument to func */ \
-register void (* _func)(void *) asm("r1") = func; /* ensure the compiler does not place this in a frame pointer register */ \
+#define BOOTSTRAP_CONTEXT(bufto, buffrom, func) do { \
+register void * _bufto asm("r0") = bufto; /* ensure the compiler places this where it will be the argument to func */ \
+register void * _buffrom asm("r4") = buffrom; /* ensure the compiler does not place this in a frame pointer register */ \
+register void (* _func)(void *) asm("r5") = func; /* ensure the compiler does not place this in a frame pointer register */ \
 asm volatile( \
 ".balign 4\n" /* pc-relative adds implicitly round down to 4-byte alignment at runtime */ \
 "add lr, pc, (0f - 1f) | 1\n" /* compute address of end of this block of asm, which will be jumped to when returning to this context */ \
 "1: push {r7, lr}\n" /* save the future pc value as well as possible frame pointer (which is not allowed in the clobber list) */ \
-"str sp, [%0]\n" /* store the current stack pointer in the context buffer */ \
+"str sp, [%1]\n" /* store the current stack pointer in the context buffer */ \
 "mov sp, %0\n" /* set the stack pointer to the top of the space below the context buffer */ \
-"bx %1\n" /* jump to the child function */ \
+"bx %2\n" /* jump to the child function */ \
 ".balign 4\n" \
-"0:\n" : "+r"(_buf), "+r"(_func) : : "r2", "r3", "r4", "r5", "r6", "r8", "r9", "r10", "r11", "r12", "lr", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "cc", "memory"); } while(0)
+"0:\n" : "+r"(_bufto), "+r"(_buffrom), "+r"(_func) : : "r1", "r2", "r3", "r6", "r8", "r9", "r10", "r11", "r12", "lr", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "cc", "memory"); } while(0)
 
-#define SWAP_CONTEXT(buf) do { \
-register void * _buf asm("r0") = buf; \
+#define SWAP_CONTEXT(bufto, buffrom) do { \
+register void * _bufto asm("r4") = bufto; \
+register void * _buffrom asm("r5") = buffrom; \
 asm volatile( \
 ".balign 4\n" \
 "add lr, pc, (0f - 1f) | 1\n" /* compute address of end of this block of asm, which will be jumped to when returning to this context */ \
 "1: push {r7, lr}\n" /* save the future pc value and possible frame pointer */ \
 "ldr r6, [%0]\n" /* load the saved stack pointer from the context buffer */ \
-"str sp, [%0]\n" /* store the current stack pointer in the context buffer */ \
+"str sp, [%1]\n" /* store the current stack pointer in the context buffer */ \
 "mov sp, r6\n" /* restore the previously saved stack pointer */ \
 "pop {r7, pc}\n" /* jump to the previously saved pc value */ \
 ".balign 4\n" \
-"0:\n" : "+r"(_buf) : : "r1", "r2", "r3", "r4", "r5", "r6", "r8", "r9", "r10", "r11", "r12", "lr", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "cc", "memory"); } while(0)
+"0:\n" : "+r"(_bufto), "+r"(_buffrom) : : "r0", "r1", "r2", "r3", "r6", "r8", "r9", "r10", "r11", "r12", "lr", "q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "cc", "memory"); } while(0)
 
-static struct child_context * current_child = NULL;
+static struct child_context main_context;
+
+static struct child_context * current_child = &main_context;
 
 /* a singly-linked list of active child tasks */
-static struct child_context * children_head = NULL;
+static struct child_context * children_head = &main_context;
 
 void yield(void) {
-    /* if in a child, yielding back to parent... */
-    if (current_child) {
-        struct child_context * tmp = current_child;
-        current_child = NULL;
-        SWAP_CONTEXT(tmp->context);
-    } else {
-        /* when yielding from parent, sleep until the next event (i.e. interrupt) */
-        sleep_until_event();
+    struct child_context * prior = current_child;
+    current_child = current_child->next;
 
-        /* loop over singly-linked list of active tasks, yielding to each */
-        for (struct child_context * this = children_head; this; this = this->next) {
-            current_child = this;
-            SWAP_CONTEXT(current_child->context);
-        }
+    if (!current_child) {
+        sleep_until_event();
+        current_child = children_head;
     }
+
+    if (prior != current_child)
+        SWAP_CONTEXT(current_child->context, prior->context);
 }
 
 /* application MAY override this if desired */
@@ -99,7 +98,7 @@ void child_start(struct child_context * child, void (* func)(void)) {
 
     child->func = func;
     current_child = child;
-    BOOTSTRAP_CONTEXT(child->context, springboard);
+    BOOTSTRAP_CONTEXT(child->context, &main_context, springboard);
 }
 
 int child_is_running(struct child_context * child) {
